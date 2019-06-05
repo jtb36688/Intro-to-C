3d7c6de136976a9b0d278f6ae6f18207542d1941
Jacob Bryan <jtb36688@gmail.com>
2019-06-05 00:01:42 -0400

    working out first pass solution to fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -5,12 +5,32 @@
     iterate n times. If the ith iteration is divisible by 3, print
     "Fizz". If it's divisible by 5, print "Buzz". If it's divisible
     by both 3 and 5, print "FizzBuzz". Increment the counter variable
-    every time that nothing gets printed and return the counter.
-    Don't forget to include newlines '\n' in your printf statements!
+    every  
+    n't forget to include newlines '\n' in your printf statements!
 */
 int fizzbuzz(int n)
 {
-
+    int count = 0;
+    char *fizzbuzz = "FizzBuzz";
+    char *fizz = "Fizz";
+    char *buzz = "Buzz";
+    void printstring(int *str, int len) {
+        for (int i = 0; i < len; i++)
+            printf("%c", *(str + i))
+    }
+    for (int x = 0; x < n; x++) {
+        count ++;
+        if ( (count % 3 == 0) && (count % 5 == 0) ) {
+            printf("FizzBuzz");
+            continue;
+        }
+        else if (count % 3 == 0) {
+            printf("Fizz");
+        }
+        else if (count % 5 == 0) {
+            printf("Buzz");
+        }
+}
 }
 
 #ifndef TESTING
